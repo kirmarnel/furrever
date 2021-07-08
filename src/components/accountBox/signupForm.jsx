28172e21cfd7c4406f1c6e00678565cdeb4a1a39
Kirsten Nelson <kirmarnel@gmail.com>
2021-07-08 11:51:13 -0700

    signup functionality

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Axios from 'axios';
 import {
   BoldLink,
   BoxContainer,
@@ -10,19 +11,33 @@ import {
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
 
+
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const register = () => {
+    Axios.post('http://localhost:3001/', {
+      fullName: fullName,
+      email: email,
+      password: password
+    }).then((res) => {
+      console.log(res)
+    })
+  }
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+        <Input type="text" placeholder="Full Name" onChange={(e) => {setFullName(e.target.value)}} />
+        <Input type="email" placeholder="Email" onChange={(e) => {setEmail(e.target.value)}} />
+        <Input type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} />
         <Input type="password" placeholder="Confirm Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={15} />
-      <SubmitButton type="submit">Signup</SubmitButton>
+      <SubmitButton type="submit" onClick={register}>Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
