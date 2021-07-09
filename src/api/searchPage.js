f65072d1607a4d65459adc790a68aa401dd9bece
Joseph Toups <josephtoups95@gmail.com>
2021-07-09 11:12:26 -0700

    updates api token & TS
    updates API token, remove/uninstall TS dependencies and config

---
+++ src/api/searchPage.js
@@ -0,0 +1,69 @@
+// import {token} from 'src/api/token.js';
+
+// var url = "https://api.petfinder.com/v2/animals"
+
+// var searchRequest = new XMLHttpRequest();
+
+// searchRequest.open("GET", url);
+
+// searchRequest.setRequestHeader("Authorization", "Bearer"+`${token}`);
+
+// searchRequest.onreadystatechange = function () {
+//    if (searchRequest.readyState === 4) {
+//       console.log(searchRequest.status);
+//       console.log(searchRequest.responseText);
+//    }};
+
+// searchRequest.send();
+
+
+// const userResponse = [
+//     {name: 'dog'},
+//     {coat: ''},
+//     {color: ''},
+//     {breed: ''},
+// ];
+
+// // onSelect --> radial buttons --> grab through props --> pass through to search 
+
+// //Filter JSON data using user responses 
+// var searchRequest = JSON.parse(this.response).filter
+
+
+// (function (userResponse) {
+//     return userResponse.id === '';
+// })
+
+
+// //GET ID
+// var id = this.response.animals.forEach(
+//     console.log(` -- id: ${animal.id}`)
+// )
+
+// //USE ID TO RENDER PET-SPECIFIC PAGE
+
+
+
+// var breed = this.response.animals.find( item =>{
+//     return item.name === "Breed"
+// })
+
+// var size = this.response.animals.find(item =>{ 
+//     return item.name === "Breed", item.name === "Size", item.name === "Gender", item.name === "Age", item.name === "Color", item.name === "Coat"
+// });
+
+// var gender = this.response.animals.find(item =>{
+//     return item.name === "Gender"
+// })
+
+// var age = this.response.animals.find(item =>{
+//     return item.name === "Age"
+// })
+
+// var color = this.response.animals.find(item =>{
+//     return item.name === "Color"
+// })
+
+// var coat = this.response.animals.find( item =>{
+//     return item.name === "Coat"
+// })
--- src/api/token.js
+++ src/api/token.js
@@ -1,62 +1,104 @@
-
-
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 
+var axios = require('axios').default;
 
-var url = "https://api.petfinder.com/v2/oauth2/token";
+var options = {
+   method: 'POST',
+   url: 'https://api.petfinder.com/v2/oauth2/token',
+   headers: {'content-type':'application/json'},
+   data: {
+      grant_type:'client_credentials',
+      client_id: `${API_KEY}`,
+      client_secret: `${API_SECRET}`,
 
-//Create request varaible and assign XMLHttpRequest object
-const request = new XMLHttpRequest()
+   }
+};
 
-//Open connection, using GET on the endpoint
-request.open("POST", url)
+axios.request(options).then(function(response) {
+   console.log(response.data)
+}).catch(function (error){
+   console.log(error);
+});
 
-request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
-request.onreadystatechange = function () {
-   if (request.readyState === 4) {
-      console.log(request.status);
-      console.log(request.responseText);
-   }};
 
-var API_TOKEN_REQ = 'grant_type=client_credentials&client_id'+`${API_KEY}`+'&client_secret='+`${API_SECRET}`;
-request.send(API_TOKEN_REQ);
 
-//Access JSON data to retrieve Token 
-var API_TOKEN_REQ = JSON.parse(this.response);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var url = "https://api.petfinder.com/v2/oauth2/token";
+
+// //Create request varaible and assign XMLHttpRequest object
+// const request = new XMLHttpRequest()
+
+// //Open connection, using GET on the endpoint
+// request.open("POST", url)
+
+// request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+
+// request.onreadystatechange = function () {
+//    if (request.readyState === 4) {
+//       console.log(request.status);
+//       console.log(request.responseText);
+//    }};
+
+// var API_TOKEN_REQ = 'grant_type=client_credentials&client_id'+`${API_KEY}`+'&client_secret='+`${API_SECRET}`;
+// request.send(API_TOKEN_REQ);
+
+// //Access JSON data to retrieve Token 
+// var API_TOKEN_RES = JSON.parse(this.response);
 // console.log(this.response.access_token);
 
-//-------NEW REQUEST FOR PARAMETERS-----------
 
-//Create new request variable for types and assign object to get search paramaters from PetFinder using token 
-var url = "https://api.petfinder.com/v2/types";
+// export let token = API_TOKEN_RES.access_token;
 
-const newRequest = new XMLHttpRequest();
+// //-------NEW REQUEST FOR SEARCH PARAMETERS-----------
 
-//Open connection 
-newRequest.open("GET", url);
+// //Create new request variable for types and assign object to get search paramaters from PetFinder using token 
+// var url = "https://api.petfinder.com/v2/types";
 
-newRequest.setRequestHeader("Authorization", "Bearer"+`${API_TOKEN_REQ.access_token}`);
+// const newRequest = new XMLHttpRequest();
 
-newRequest.onreadystatechange = function () {
-   if (newRequest.readyState === 4) {
-      console.log(newRequest.status);
-      console.log(newRequest.responseText);
-   }};
+// //Open connection 
+// newRequest.open("GET", url);
 
-newRequest.send();
+// newRequest.setRequestHeader("Authorization", "Bearer"+`${API_TOKEN_RES.access_token}`);
 
-//Access JSON data to retrieve parameters
-var newRequest = JSON.parse(this.response)
+// newRequest.onreadystatechange = function () {
+//    if (newRequest.readyState === 4) {
+//       console.log(newRequest.status);
+//       console.log(newRequest.responseText);
+//    }};
 
-//Find and return only the items from types.name.dog
-var dog = this.response.types.find(item =>{ 
-  return item.name === "Dog" //returns coats, colors, gender
-})
+// newRequest.send();
+
+// //Access JSON data to retrieve parameters
+// var newRequest = JSON.parse(this.response)
+
+// //Find and return only the items from types.name.dog
+// var dog = this.response.type.find(item =>{ 
+//   return item.name === "Dog" //returns coats, colors, gender, and breeds in search page 
+// })
+
+// //EXPORT/RENDER RESULTS TO SEARCH PAGE, MAKE SELECTABLE, THEN SEARCH PAGE TAKES SELECTIONS AND RETURNS MATCHING ANIMALS 
 
 
-// -------------------------------------------------------------------
+// // -------------------------------------------------------------------
 
 
 
