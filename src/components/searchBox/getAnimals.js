3cc2bc4bbe9a5c0ad526f4ef8a26ecb593ebc492
Joseph Toups <josephtoups95@gmail.com>
2021-07-15 16:40:48 -0700

    API, Card, and Search Changes --working
    Changes made to Card.js and Search.js. Search.js now contains the API calls, and then renders the response in Card.js

--- src/components/searchBox/getAnimals.js
+++ src/components/searchBox/getAnimals.js
@@ -1,9 +1,9 @@
 const petfinder = require("@petfinder/petfinder-js")
+
 const API_KEY = 'L2PfxvEeu4R2F4FxiieGEHkh78o1dQpEapxnWjesdIvACBSsWP'
 const API_SECRET = 'F9tVif6NQRsjR6XHKkgTiov9bd2DujsyYEjqaEvB'
  
 
-const breedToSearch = ('Corgi') //2dogDropDown
 
 var client = new petfinder.Client({apiKey: API_KEY, secret: API_SECRET})
 
@@ -11,7 +11,7 @@ async function showAnimals(animalType, searchBreed) {
 
   let page = 1;
 
-  do {
+  // do {
     var apiResult = await client.animal.search({
       type: animalType,
       breed: searchBreed,
@@ -23,19 +23,22 @@ async function showAnimals(animalType, searchBreed) {
 
     apiResult.data.animals.forEach(function(animal) {
 
-      console.log(` -- ${++dogIdx}: ${animal.name} id: ${animal.id} url: ${animal.url}`);
+      // console.log(` -- ${++dogIdx}: ${animal.name} id: ${animal.id} url: ${animal.url}`);
       // console.log(animal)
 
     });
 
     page++;
+    return apiResult
 
-  } while(apiResult.data.pagination && apiResult.data.pagination.total_pages >= page);
+  // } while(apiResult.data.pagination && apiResult.data.pagination.total_pages >= page);
+  
 }
 
 
-async function populateAnimals() {
-  await showAnimals("Dog", `${breedToSearch}`); //Need to pass in user selection from dropdown 
+async function populateAnimals(dog) {
+  let result = await showAnimals("Dog", dog); //Need to pass in user selection from dropdown 
+   return result;
 };
 
 export default populateAnimals;
