ad6f73314fece75055d96b285e7fcae8ac386e79
Joseph Toups <josephtoups95@gmail.com>
2021-07-19 13:26:51 -0700

    cleaning code, adding comments

--- src/components/Pages/Card.js
+++ src/components/Pages/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import styled from "styled-components"
 import Thumbup from '../assets/thumbsup.png';
 import Thumbdown from '../assets/thumbsdown.png';
-import { Search } from "../Pages/Search";
 
+
+//Styling
 
 const InnerText = styled.h2`
   font-size: 25px;
@@ -26,113 +26,6 @@ const InnerContainer = styled.div`
 
 `;
 
-const Input = styled.input`
-  width: 100%;
-  height: 42px;
-  outline: none;
-  border: 1px solid rgba(200, 200, 200, 0.3);
-  padding: 0px 10px;
-  border-bottom: 1.4px solid transparent;
-  transition: all 200ms ease-in-out;
-  font-size: 12px;
-
-  &::placeholder {
-    color: rgba(200, 200, 200, 1);
-  }
-
-  &:not(:last-of-type) {
-    border-bottom: 1.5px solid rgba(200, 200, 200, 0.4) 20;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: 2px solid rgb(220, 106, 106, 1);
-  }
-`;
-const Button = styled.button`
-  
-  top-margin: 10px;
-  padding: 10px 40%;
-  color: #fff;
-  font-size: 15px;
-  font-weight: 600;
-  border: none;
-  border-radius: 100px 100px 100px 100px;
-  cursor: pointer;
-  transition: all, 240ms ease-in-out;
-  background: rgb(241, 196, 15);
-  background: linear-gradient(
-    58deg,
-    rgba(220, 106, 106, 1) 20%,
-    rgba(198, 112, 112, 1) 100%
-  );
-
-  &:hover {
-    filter: brightness(1.03);
-  }
-`;
-const SubmitButton = styled.button`
-  
-  top-margin: 10px;
-  padding: 10px 40%;
-  color: #fff;y
-  font-size: 15px;
-  font-weight: 600;
-  border: none;
-  border-radius: 100px 100px 100px 100px;
-  cursor: pointer;
-  transition: all, 240ms ease-in-out;
-  &:hover {
-    filter: brightness(1.03);
-  }
-`;
-const Select = styled.select`
-  width: 100%;
-  height: 42px;
-  outline: none;
-  border: 1px solid rgba(200, 200, 200, 0.3);
-  padding: 0px 10px;
-  border-bottom: 1.4px solid transparent;
-  transition: all 200ms ease-in-out;
-  font-size: 12px;
-  color: #afada2;
-  &::placeholder {
-    color: rgba(200, 200, 200, 1);
-  }
-
-  &:not(:last-of-type) {
-    border-bottom: 1.5px solid rgba(200, 200, 200, 0.4) 20;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: 2px solid rgb(220, 106, 106, 1);
-  }
-`;
-
-const Textarea = styled.textarea`
-  width: 100%;
-  height: 42px;
-  outline: none;
-  border: 1px solid rgba(200, 200, 200, 0.3);
-  padding: 0px 10px;
-  border-bottom: 1.4px solid transparent;
-  transition: all 200ms ease-in-out;
-  font-size: 12px;
-
-  &::placeholder {
-    color: rgba(200, 200, 200, 1);
-  }
-
-  &:not(:last-of-type) {
-    border-bottom: 1.5px solid rgba(200, 200, 200, 0.4) 20;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: 2px solid rgb(220, 106, 106, 1);
-  }
-`;
 const LableContainer = styled.div`
 width: 250px;
 display: flex;
@@ -145,10 +38,6 @@ align-items: center;
 font-family:apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
 `;
 
-const Title = styled.h2`
-  color: #fff;
-  font-weight: 300;
-`
 const Date = styled.div`
   color: #ccc;
   font-weight: 300;
@@ -158,73 +47,68 @@ const Description = styled.p`
   color: #737373;
   font-weight: 300;
 
+`;
+
+const ActionButton = styled.button`
+display:inline-block;
+text-weight: 10px;
+margin-top: 50px;
+margin-left: 10px;
+padding: 8px 14px 10px 10px;
+border: none;
+background: #ffff;
+color: #fff;
+border-radius: 80px 80px 80px 80px;
+cursor: pointer;
+:hover {
+  opacity: 0.8;
   `;
 
-  const ActionButton = styled.button`
-  display:inline-block;
-  text-weight: 10px;
-  margin-top: 50px;
-  margin-left: 10px;
-  padding: 8px 14px 10px 10px;
-  border: none;
-  background: #ffff;
-  color: #fff;
-  border-radius: 80px 80px 80px 80px;
-  cursor: pointer;
-  :hover {
-    opacity: 0.8;
-    `;
+const Container = styled.div`
+background-size: cover;
+background-position: center;
+background-clip: content-box;
+border: 3px solid white;
+height: 200px;
+width: 250px;
+box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+position: relative;
+margin: 0 auto;
+margin-top: 5px;
+border-radius: 19px;
+`;
 
-  const Container = styled.div`
-  background-size: cover;
-  background-position: center;
-  background-clip: content-box;
-  border: 3px solid white;
-  height: 200px;
-  width: 250px;
-  box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
-  position: relative;
-  margin: 0 auto;
-  margin-top: 5px;
-  border-radius: 19px;
- `;
-
- const Wrapper = styled.div`
-  float: left;
-  width: 100%;
-  text-align: center;
-  position: relative;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+const Wrapper = styled.div`
+float: left;
+width: 100%;
+text-align: center;
+position: relative;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
 `
-    
 
-
+//Apply styling to data returned from API for client-side
 
 export const Card = (props) => {
 console.log(props);
   return (
-      <InnerContainer>
-        <LableContainer>
-    <InnerText>{props.props.name} </InnerText>
-
-    <Container>
-      <img src={props.props.photos} alt={props.props.photos}
-      />
-    </Container>
-    <Date></Date>
-    <Description>
-      {props.props.description}
-    </Description>
-    <br>
-    </br>
-    <Wrapper>
-    <ActionButton ><img src = {Thumbup} height={50} width={50} alt="up"/></ActionButton>
-    <ActionButton ><img src = {Thumbdown} height={50} width={50} alt="down"/></ActionButton>
-    </Wrapper>  
-          </LableContainer>
-          </InnerContainer>
-
+    <InnerContainer>
+      <LableContainer>
+        <InnerText>{props.props.name} </InnerText>
+        <Container>
+          <img src={props.props.photos} alt={props.props.photos}/>
+        </Container>
+        <Date></Date>
+        <Description>
+          {props.props.description}
+        </Description>
+        <br></br>
+        <Wrapper>
+          <ActionButton ><img src = {Thumbup} height={50} width={50} alt="up"/></ActionButton>
+          <ActionButton ><img src = {Thumbdown} height={50} width={50} alt="down"/></ActionButton>
+        </Wrapper>  
+      </LableContainer>
+    </InnerContainer>
   );
 };
--- src/components/Pages/Search.js
+++ src/components/Pages/Search.js
@@ -1,66 +1,13 @@
 // import { Button } from "bootstrap";
 import React from "react";
-import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { Card } from "../Pages/Card";
 const petfinder = require("@petfinder/petfinder-js")
 
-const InnerText = styled.h2`
-  font-size: 25px;
-  text-align: center;
-  font-weight: 800;
-  color: #dc6a6a;
-  padding: 20pt;
-  font-family:apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-`;
 
-const Title = styled.div`
-font-size: 10pt;
-color: #737373;
-font-family:apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-`;
+//Styling
 
 
-const InnerContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 0 1.8em;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-
-`;
-
-const BottomContainer = styled.div`
-height:450px;
-display: flex;
-`;
-
-
-const Input = styled.input`
-  width: 100%;
-  height: 42px;
-  outline: none;
-  border: 1px solid rgba(200, 200, 200, 0.3);
-  padding: 0px 10px;
-  border-bottom: 1.4px solid transparent;
-  transition: all 200ms ease-in-out;
-  font-size: 12px;
-
-  &::placeholder {
-    color: rgba(200, 200, 200, 1);
-  }
-
-  &:not(:last-of-type) {
-    border-bottom: 1.5px solid rgba(200, 200, 200, 0.4) 20;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: 2px solid rgb(220, 106, 106, 1);
-  }
-`;
 const SubmitButton = styled.button`
   width: 100%;
   top-margin: 10px;
@@ -83,6 +30,7 @@ const SubmitButton = styled.button`
     filter: brightness(1.03);
   }
 `;
+
 const Select = styled.select`
   width: 100%;
   height: 42px;
@@ -107,29 +55,6 @@ const Select = styled.select`
   }
 `;
 
-const Textarea = styled.textarea`
-  width: 100%;
-  height: 42px;
-  outline: none;
-  border: 1px solid rgba(200, 200, 200, 0.3);
-  padding: 0px 10px;
-  border-bottom: 1.4px solid transparent;
-  transition: all 200ms ease-in-out;
-  font-size: 12px;
-
-  &::placeholder {
-    color: rgba(200, 200, 200, 1);
-  }
-
-  &:not(:last-of-type) {
-    border-bottom: 1.5px solid rgba(200, 200, 200, 0.4) 20;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: 2px solid rgb(220, 106, 106, 1);
-  }
-`;
 const LabelContainer = styled.div`
 width: 250px;
 display: flex;
@@ -142,86 +67,86 @@ align-items: center;
 font-family:apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
 `;
 
+
+//Class takes user selection, feeds it into the API call, API returns information and photos (if available), then pushes to Card.js. 
+
+
 class Search extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {dog:'Labrador Retriever', 
-      apiData:[],
-      currentPet: {},
-      currentPetIndex: 0,
-      displaySearch: true
-    };
+  constructor(props) {
+    super(props);
+    this.state = {dog:'Labrador Retriever', 
+    apiData:[],
+    currentPet: {},
+    currentPetIndex: 0,
+    displaySearch: true
+  };
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({dog: event.target.value});
+    event.preventDefault(); 
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const API_KEY = '0Hj8w8pgamlFFvo6xp8aJG6GPVcIb0tCwYv26pzW6AKvRf7PlT'
+    const API_SECRET = 'OG9M2CYbc8EMQBDAFZHav5TUMmGO1xYM9bCsB2pa'
+    var client = new petfinder.Client({apiKey: API_KEY, secret: API_SECRET})
+
+      client.animal.search({
+        type: "Dog",
+        breed: this.state.dog,
+        limit: 10,
+      }).then(response => {
+        console.log(response.data.animals);
+        var apiData = response.data.animals;
+        let apiResults = []
+        for (let i=0;i < apiData.length;i++) {
+          if (apiData [i].photos[0]){
+          apiResults.push({
+            name: apiData[i].name,
+            description: apiData[i].description, 
+            photos: apiData[i].photos[0].small
+          })
+        }else if (apiData[i].primary_photo_cropped){
+          apiResults.push({
+            name: apiData[i].name,
+            description: apiData[i].description, 
+            photos: apiData[i].primary_photo_cropped.small
+          })
+        } else {
+          apiResults.push({
+            name: apiData[i].name,
+            description: apiData[i].description, 
+            photos: "Sorry, this dog has no photos",
+          })
+        }
+        }
+      this.setState({apiData:apiResults,currentPet:apiResults[0],displaySearch:false});
+      console.log(this.state.apiData)
+    });
     
-  
-      // this.handleChange = this.handleChange.bind(this);
-      // this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange = (event) => {
-      console.log(event.target.value)
-      this.setState({dog: event.target.value});
-      event.preventDefault(); 
-    }
-  
-    handleSubmit = (event) => {
-      event.preventDefault();
-      const API_KEY = '0Hj8w8pgamlFFvo6xp8aJG6GPVcIb0tCwYv26pzW6AKvRf7PlT'
-      const API_SECRET = 'OG9M2CYbc8EMQBDAFZHav5TUMmGO1xYM9bCsB2pa'
-      var client = new petfinder.Client({apiKey: API_KEY, secret: API_SECRET})
-
-       client.animal.search({
-          type: "Dog",
-          breed: this.state.dog,
-          limit: 10,
-        }).then(response => {
-          console.log(response.data.animals);
-          var apiData = response.data.animals;
-          let apiResults = []
-          for (let i=0;i < apiData.length;i++) {
-            if (apiData [i].photos[0]){
-            apiResults.push({
-              name: apiData[i].name,
-              description: apiData[i].description, 
-              photos: apiData[i].photos[0].small
-            })
-          }else if (apiData[i].primary_photo_cropped.small){
-            apiResults.push({
-              name: apiData[i].name,
-              description: apiData[i].description, 
-              photos: apiData[i].primary_photo_cropped.small
-            })
-          } else {
-            apiResults.push({
-              name: apiData[i].name,
-              description: apiData[i].description, 
-              photos: "",
-            })
-          }
-          }
-        this.setState({apiData:apiResults,currentPet:apiResults[0],displaySearch:false});
-        console.log(this.state.apiData)
-      });
-      
-    }
-    nextPet= () =>{
-      let currentPetIndex = this.state.currentPetIndex
-      if( this.state.currentPetIndex < this.state.apiData.length-1){
-        currentPetIndex++
-         this.setState({currentPetIndex:currentPetIndex, 
-           currentPet: this.state.apiData[currentPetIndex]})
-
-      
-      }else{
-        currentPetIndex = 0;
+  }
+  nextPet= () =>{
+    let currentPetIndex = this.state.currentPetIndex
+    if( this.state.currentPetIndex < this.state.apiData.length-1){
+      currentPetIndex++
         this.setState({currentPetIndex:currentPetIndex, 
           currentPet: this.state.apiData[currentPetIndex]})
-      }
+
+    
+    }else{
+      currentPetIndex = 0;
+      this.setState({currentPetIndex:currentPetIndex, 
+        currentPet: this.state.apiData[currentPetIndex]})
     }
-  
-    render() {
-      return (
-        <div>
-          {this.state.displaySearch?
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.displaySearch?
         <form>
           <LabelContainer>
             Select a Breed:
@@ -240,23 +165,19 @@ class Search extends React.Component {
               <option value="Terrier">Terrier</option>
             </Select>
           </LabelContainer>
-          <br>
-          </br>
-          <br>
-          </br>
-          <br>
-          </br>
+          <br></br>
+          <br></br>
+          <br></br>
           <SubmitButton type="submit" onClick={this.handleSubmit}>Search Dogs</SubmitButton>
-
         </form>
-        :<>
-        <Card props={this.state.currentPet} />
-        <button onClick={this.nextPet}>Goto next pet</button></>}
-        {/* {this.state.apiData.map((i, key) => <Card props = {i} key={key}/>)} */}
 
-        </div>
-      )
-    };
+      :<>
+      
+      <Card props={this.state.currentPet} />
+      <button onClick={this.nextPet}>NEXT</button></>}
+      </div>
+    )
+  };
 }
   
-  export default Search
+export default Search
